9bd051f32a27daf45f136ee7a25124b29c66e150
Hanqi1010 <lihanqitongxue@gmail.com>
2024-12-19 09:44:43 -0600

    Assignment 2

--- Assignments/Assignment02_Hanqi/buffer.py
+++ Assignments/Assignment02_Hanqi/buffer.py
@@ -1,7 +1,7 @@
 import arcpy
 arcpy.analysis.Buffer(
-    in_features="no_retail.shp",
-    out_feature_class="./retail_buffer_1.shp",
+    in_features="F:/OneDrive - Louisiana State University/LSU_Course/GEOG/GEOG 4057 GIS Programming/GEOG4057_Hanqi/Assignments/Assignment02_Hanqi/DataFolder/no_retail.shp",
+    out_feature_class="F:/OneDrive - Louisiana State University/LSU_Course/GEOG/GEOG 4057 GIS Programming/GEOG4057_Hanqi/Assignments/Assignment02_Hanqi/DataFolder/retail_buffer_1.shp",
     buffer_distance_or_field="500 Meters",
     line_side="FULL",
     line_end_type="ROUND",
